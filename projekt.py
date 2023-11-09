2c622999a7e9e2ca1ff66a11edddf0ac2dffb8b9
Przemek7689 <przemek.kaleta@interia.pl>
2023-11-09 10:25:15 +0100

    .

---
+++ projekt.py
@@ -0,0 +1,9 @@
+print("Witaj w LOTTO")
+a=int(input("Podaj liczbe 1:  "))
+b=int(input("Podaj liczbe 2:  "))
+c=int(input("Podaj liczbe 3:  "))
+d=int(input("Podaj liczbe 4:  "))
+e=int(input("Podaj liczbe 5:  "))
+f=int(input("Podaj liczbe 6:  "))
+Lista=[a,b,c,d,e,f]
+for i in range(0,5):
