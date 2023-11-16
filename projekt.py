91ffbb048b428f5598a001c7aa58d74c5b351c22
Przemek7689 <przemek.kaleta@interia.pl>
2023-11-16 09:34:38 +0100

    .

--- projekt.py
+++ projekt.py
@@ -1,7 +1,7 @@
 print("Witaj w LOTTO")
 print("Podaj 6 losowych liczb, pamietaj aby sie nie powtarzaly")
 Lista2 = [3,4,5,6,7,11]
-numer = 0
+numer = 1
 a=int(input("Podaj liczbe 1:  "))
 b=int(input("Podaj liczbe 2:  "))
 c=int(input("Podaj liczbe 3:  "))
@@ -12,7 +12,7 @@ Lista=[a,b,c,d,e,f]
 for g in range(0,5):
     for i in range(0,5):
             if Lista[g]==Lista2[i]:
-                numer = numer + 1
+                numer = numer + 1 
 
 print("Trafiłeś",  " ",numer)
 
