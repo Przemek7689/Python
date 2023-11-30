a32877a94250953623a12cb536981a1581e92ee4
Przemek7689 <przemek.kaleta@interia.pl>
2023-11-30 09:57:22 +0100

    .

---
+++ wisielec.py
@@ -0,0 +1,43 @@
+import random
+
+
+print("Witaj w grze wisielca, podaj swoje imię: ")
+imie = input()
+
+
+lista = ["telefon", "ksiazka", "marker", "neandertalczyk", "zabawka"]
+
+haslo = str(lista[random.randint(0, len(lista) - 1)])
+tablica = list(haslo)
+   
+for i in range(len(haslo)):
+    tablica[i] = "_"
+
+zycie = 10
+
+while zycie > 0:
+    print("")
+    print(imie, " pozostalo ci ", zycie, " zyc")
+    print("")
+    print(" ".join(tablica))
+    print(" ")
+
+
+    print("Podaj swoja litere: ")
+    litera = input()
+
+   
+    if litera in haslo:
+        for i in range(len(haslo)):
+            if haslo[i] == litera:
+                tablica[i] = litera
+        if "".join(map(str, tablica)) == haslo:
+            print("")
+            print(imie, " pozostalo ci ", zycie, " zyc")
+            print("")
+            print(" ".join(tablica))
+            print(" ")
+            print(imie, " wygrales!")
+    else:
+        zycie -= 1
+        print("Niestety tym razem się nie udało")
